8c0243cbd0165ca375bfff716509935622102c3c
unknown <kopanomasuku@gmail.com>
2023-05-31 20:27:35 +0200

    TDD

---
+++ isFromBellvile.js
@@ -0,0 +1,5 @@
+function isFromBellville(registrationNumber){
+    return(registrationNumber.startsWith("CY"));  
+    }
+    //console.log(isFromBellville("CY659NW"));
+    console.log(isFromBellville("CB659NW"));
